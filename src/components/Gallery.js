c01c1dcc0ad69eae5e241d444f6aa77a1282afbc
Rudi <rudidreyer7@gmail.com>
2021-11-15 16:49:25 +0200

    Images can now be displayed

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,16 +1,43 @@
-import React from 'react'
-import { Navigate } from 'react-router'
+import React, { useState, useEffect } from 'react';
+// import { Navigate } from 'react-router';
+import { Image } from 'react-bootstrap';
+import axios from 'axios';
 
 const Gallery = (props) => {
 
-    if (!props.loggedIn) {
-        return <Navigate to="/login" />
-    }
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        let email = localStorage.getItem('email');
+        let token = localStorage.getItem('token');
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+
+        axios.get("http://192.168.3.228:8060/image/fetch/" + email, {
+            headers: headers
+        }).then(response => {
+            console.log(response.data);
+            setImages(response.data.payload);
+        }).catch(error => console.log(error.message));
+    }, []);
+
+    // if (!props.loggedIn) {
+    //     return <Navigate to="/login" />
+    // }
 
     return (
-        <div className="text-black">
-            Gallery
-        </div>
+        <>
+            {images.map(image => (
+                <div key={image.id}>
+                    <p>{image.metadata.originalFileName}</p>
+                    <Image name={image.url} src={"http://192.168.3.228:8060/image/18/display/" + image.url}
+                    onClick={(e) => alert(e.target.name)} width="150px" height="150px" />
+                </div>
+            ))}
+        </>
     )
 }
 
