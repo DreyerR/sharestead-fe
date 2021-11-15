f6030e877bac523abefd89d9e953509a25b5bce8
Rudi <rudidreyer7@gmail.com>
2021-11-15 20:38:29 +0200

    Added some styling when displaying images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-// import { Navigate } from 'react-router';
-import { Image } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 
 const Gallery = (props) => {
 
     const [images, setImages] = useState([]);
+    let userId = localStorage.getItem('userId');
 
     useEffect(() => {
+        loadImages();
+    }, []);
+
+    const loadImages = () => {
         let email = localStorage.getItem('email');
         let token = localStorage.getItem('token');
 
@@ -16,28 +20,39 @@ const Gallery = (props) => {
             'Authorization': 'Bearer ' + token
         }
 
-        axios.get("http://192.168.3.228:8060/image/fetch/" + email, {
+        axios.get(`http://localhost:8060/image/fetch/${email}`, {
             headers: headers
         }).then(response => {
-            console.log(response.data);
+
             setImages(response.data.payload);
         }).catch(error => console.log(error.message));
-    }, []);
-
-    // if (!props.loggedIn) {
-    //     return <Navigate to="/login" />
-    // }
+    }
 
     return (
-        <>
-            {images.map(image => (
-                <div key={image.id}>
-                    <p>{image.metadata.originalFileName}</p>
-                    <Image name={image.url} src={"http://192.168.3.228:8060/image/18/display/" + image.url}
-                    onClick={(e) => alert(e.target.name)} width="150px" height="150px" />
-                </div>
-            ))}
-        </>
+        <div>
+            <Container>
+                <Row>
+                    {images.length > 0 ? images.map(image => (
+                        <Col key={image.id} lg={4} md={6} sm={12}>
+                            <Card style={{ marginBottom: '20px' }}>
+                                <Card.Img className="mx-auto" style={{ width: '90%', height: '250px', objectFit: 'contain', cursor: 'pointer' }} variant="top"
+                                    src={`http://localhost:8060/image/${userId}/display/${image.url}`} onClick={e => {window.open(e.target.src, '_blank').focus()}}  />
+                                <Card.Body>
+                                    <Card.Title>{image.metadata.originalFileName}</Card.Title>
+                                    <Card.Text>
+                                        <strong>Image Size:</strong> {image.metadata.imgSize} bytes <br />
+                                        <strong>Date uploaded:</strong> {image.metadata.dateCreated}
+                                    </Card.Text>
+                                    <Button variant="primary">Share</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )) : 
+                        <h1 className="text-center">No images available</h1>
+                    }
+                </Row>
+            </Container>
+        </div>
     )
 }
 
